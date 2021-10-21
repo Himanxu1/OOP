3e947365c860b2623a46fb0c10966c88661ab82f
Himanshu Singh <bt20cse062@sce.org.in>
2021-10-21 16:19:04 +0530

    Create SelectionSort.java File

---
+++ Buoi2/SelectionSort.java
@@ -0,0 +1,27 @@
+package array;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		int a[] = {5,3,7,-2,8,9};
+		int n = a.length;
+		for(int i=0; i<n-1; i++) {
+			int minInd = i;
+			for(int j=i; j<n; j++) {
+				if(a[j]<a[minInd]) {
+					minInd = j;
+				}
+			}
+			int temp = a[i];
+			a[i] = a[minInd];
+			a[minInd] = temp;
+					
+		}
+		for(int e: a) {
+			System.out.print(e+" ");
+		
+		}
+		
+	}
+
+}
